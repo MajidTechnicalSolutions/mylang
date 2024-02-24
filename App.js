ae3b390dedba8a324d53e4c57e3490b9c4501863
jamalabd <jamalabdul47@gmail.com>
2024-02-23 21:33:11 -0500

    install rnuilib

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import View from "react-native-ui-lib/view";
+import Text from "react-native-ui-lib/text";
+import {
+  KeyboardTrackingView,
+  KeyboardAwareInsetsView,
+  KeyboardRegistry,
+  KeyboardAccessoryView,
+  KeyboardUtils,
+} from "react-native-ui-lib/keyboard";
 
 export default function App() {
   return (
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
